1626d7a2cd2e6cf2780a92a845c69ad0b63520be
endorivium <jihrenberger@gmail.com>
2026-05-26 15:34:17 +0200

    added non-functional login-view

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,27 +1,44 @@
-import {
-  Button
-} from "@mui/material";
 import { useState } from "react";
 import './Login.css'
+import { Button } from "@mui/material";
 
-function LoginButton(){
-  return(
-    <div className="login-button">
-    <Button variant="contained">Login</Button>
-    </div>
+function LoginButton() {
+  const [showLoginView, setShowLoginView] = useState(false);
+  const onClick = () => setShowLoginView(!showLoginView);
+  return (
+    <>
+      <div className="login-button">
+        <Button variant="contained" onClick={onClick}>Login</Button>
+      </div>
+      <div className="login-view-container">
+        {showLoginView ? <LoginView /> : null}
+      </div>
+    </>
   );
 }
 
-function Login() {
-  const [email, setEmail] = useState("");
+function LoginView() {
+  const [email, setEmail] = useState("email@domain.com");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {};
+  // const handleLogin = () => {};
 
   return (
-    <>
-    
-    </>
+    <div className="login-wrapper">
+        <form>
+          <label><p>Email</p>
+            <input type="text"
+              onChange={(e) => setEmail(e.target.value)}></input>
+          </label>
+          <label><p>Password</p>
+            <input type="text"
+              onChange={(e) => setPassword(e.target.value)}></input>
+          </label>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
   );
 };
 
